cc21f7d7d7b64f94b1df43333ff413eb3e52f475
(메가존)장진익 <mz_jangjinik@woowafriends.com>
2020-11-30 16:56:08 +0900

    Simplify Your Spring Boot App Login with GitHub Single Sign-on

--- src/main/java/me/jinik/tutorial/basic/basicloginspringboot/config/SecurityConfiguration.java
+++ src/main/java/me/jinik/tutorial/basic/basicloginspringboot/config/SecurityConfiguration.java
@@ -11,10 +11,12 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
+                .antMatcher("/**")
                 .authorizeRequests()
+                .antMatchers("/", "/login**").permitAll()
                 .anyRequest().authenticated()
                 .and()
-                .httpBasic();
+                .oauth2Login();
     }
 
     @Override
--- src/main/java/me/jinik/tutorial/basic/basicloginspringboot/controller/WebController.java
+++ src/main/java/me/jinik/tutorial/basic/basicloginspringboot/controller/WebController.java
@@ -1,6 +1,11 @@
 package me.jinik.tutorial.basic.basicloginspringboot.controller;
 
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
+import org.springframework.security.oauth2.client.annotation.RegisteredOAuth2AuthorizedClient;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -8,9 +13,18 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class WebController {
 
     @RequestMapping("/")
-    @ResponseBody
     public String index() {
-        return "That's pretty basic!";
+        return "index";
+    }
+
+    @RequestMapping("/securedPage")
+    public String securedPage(Model model,
+                              @RegisteredOAuth2AuthorizedClient OAuth2AuthorizedClient authorizedClient,
+                              @AuthenticationPrincipal OAuth2User oauth2User) {
+        model.addAttribute("userName", oauth2User.getName());
+        model.addAttribute("clientName", authorizedClient.getClientRegistration().getClientName());
+        model.addAttribute("userAttributes", oauth2User.getAttributes());
+        return "securedPage";
     }
 
 }
